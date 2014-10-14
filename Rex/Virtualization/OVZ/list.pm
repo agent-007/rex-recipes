3f8042a6962408c0de60ec7aed3d3469f6b40c87
Alex Gorbachenko alex.gorbachenko@gmail.com <alex.gorbachenko@gmail.com>
2014-10-14 11:10:35 +0400

    - fix vm list for OVZ.

--- Rex/Virtualization/OVZ/list.pm
+++ Rex/Virtualization/OVZ/list.pm
@@ -8,38 +8,37 @@ package Rex::Virtualization::OVZ::list;
 
 use strict;
 use warnings;
+use Carp;
 
 use Rex::Logger;
 use Rex::Helper::Run;
 
 sub execute {
 
-    my @domains;
+    my ( $class, $arg1 ) = @_;
+    
+    my @domains = ();
 
     Rex::Logger::debug("Getting OVZ container list by vzlist");
 
-    my ( $class, $arg1 ) = @_;
-
     if ( $arg1 eq "all" ) {
         @domains = i_run "vzlist -a -H";
-        if ( $? != 0 ) {
-            die("Error running vzlist -a -H");
-        }
     }
+    
     elsif ( $arg1 eq "running" ) {
         @domains = i_run "vzlist -H";
-        if ( $? != 0 ) {
-            die("Error running vzlist -H");
-        }
     }
+    
     elsif ( $arg1 eq "stopped" ) {
         @domains = i_run "vzlist -S -H";
-        if ( $? != 0 ) {
-            die("Error running vzlist -S -H");
-        }
     }
+    
     else {
-        return;
+        croak("Unknown option $arg1. Valid options: all, running, stopped");
+    }
+        
+    if ( $? != 0 ) {
+        croak("Error running vzlist. Reason: @domains");
     }
 
     return \@domains;
